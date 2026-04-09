c8bba3fa2ecb0de5db35507b7f06de4fc8ac8e01
Miraj Shrestha <mirajs@metropolia.fi>
2026-04-09 10:11:43 +0300

    fix health check

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const app = express()
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
