40eae59d3e4b8fc0e017fec80135ea69f5fcbd4a
Miraj Shrestha <mirajs@metropolia.fi>
2026-04-09 10:07:57 +0300

    break health check

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
@@ -16,3 +18,4 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
 })
+
