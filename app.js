d026fd0fd07c952bb5a59edf3a74460202b13c0b
Miraj Shrestha <mirajs@metropolia.fi>
2026-04-09 09:51:24 +0300

    add health check, version endpoints and build script

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 
-// Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000
-
 app.use(express.static('dist'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`) // eslint-disable-line no-console
